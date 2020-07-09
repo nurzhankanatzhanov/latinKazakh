bb08a24ba1adeec32d3c85535e2ba24812c21799
Nurzhan Kanatzhanov <nurzhan.kanatzhanov@wustl.edu>
2020-07-09 16:43:37 -0400

    setup

---
+++ latin-kazakh/src/Converter/Converter.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./Converter.css";
+
+class Converter extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Converter;
---
+++ latin-kazakh/src/Keyboard/Keyboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./Keyboard.css";
+
+class Keyboard extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Keyboard;
